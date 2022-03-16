563a782121d00b1d74c97351f132ec6a539f35e2
Kai Hughes <72110026+KaiHugs@users.noreply.github.com>
2022-03-15 23:30:17 -0400

    Update LinearSolverMatrix.py

--- LinearSolverMatrix.py
+++ LinearSolverMatrix.py
@@ -1,40 +1,38 @@
 import math
-def eliminate(r1, r2, col, target=0):
+def gaussian(x):
+    for i in range(len(x)):
+        if x[i][i] == 0:
+            for j in range(i+1, len(x)):
+                if x[i][j] != 0:
+                    x[i], x[j] = x[j], x[i]
+                    break
+            else:
+                raise ValueError("Not Invertible") # if inverse == matrix
+        for j in range(i+1, len(x)):
+            elim(x[i], x[j], i)
+    for i in range(len(x)-1, -1, -1):
+        for j in range(i-1, -1, -1):
+            elim(x[i], x[j], i)
+    for i in range(len(x)):
+        elim(x[i], x[i], i, target=1)
+    return x
+
+def elim(r1, r2, col, target=0):
     fac = (r2[col]-target) / r1[col]
     for i in range(len(r2)):
         r2[i] -= fac * r1[i]
 
-def gauss(a):
-    for i in range(len(a)):
-        if a[i][i] == 0:
-            for j in range(i+1, len(a)):
-                if a[i][j] != 0:
-                    a[i], a[j] = a[j], a[i]
-                    break
-            else:
-                raise ValueError("Not Invertible")
-        for j in range(i+1, len(a)):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)-1, -1, -1):
-        for j in range(i-1, -1, -1):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)):
-        eliminate(a[i], a[i], i, target=1)
-    return a
-
-def inverse(a):
+def inverse(x):
     tmp = [[] for _ in a]
-    for i,row in enumerate(a):
-        assert len(row) == len(a)
-        tmp[i].extend(row + [0]*i + [1] + [0]*(len(a)-i-1))
-    gauss(tmp)
-    ret = []
+    for i,row in enumerate(x):
+        assert len(row) == len(x)
+        tmp[i].extend(row + [0]*i + [1] + [0]*(len(x)-i-1))
+    gaussian(tmp)
+    mat = []
     for i in range(len(tmp)):
-        ret.append(tmp[i][len(tmp[i])//2:])
-    return ret
+        mat.append(tmp[i][len(tmp[i])//2:])
+    return mat
 
-# 4x  + 3y = 20
-# -5x + 9y = 26
 R = int(input("Enter the number of Equations:"))
   
 # Initialize matrix
@@ -57,24 +55,21 @@ for i in range(R):          # Second Equation
     break
 Bmatrix = []
 
-
 for i in range(R): # A for loop for row entries
     value = float(input("Enter the value of the Equations: "))
     Bmatrix.append([value])
 
-M = matrix
-A = inverse(M)
-B = Bmatrix
+A = inverse(matrix)
 
 result = [[0, 0],[0, 0]]
-for i in range(len(A)):
+for i in range(len(Bmatrix)):
  
-    # iterating by column by B
-    for j in range(len(B[0])):
+    # iterating by column 
+    for j in range(len(Bmatrix[0])):
  
-        # iterating by rows of B
-        for k in range(len(B)):
-            result[i][j] += A[i][k] * B[k][j]
+        # iterating by rows 
+        for k in range(len(Bmatrix)):
+            result[i][j] += A[i][k] * Bmatrix[k][j]
 
 print("X = ", round(result[j][j], 3))
 print("Y = ", round(result[i][j], 3))
